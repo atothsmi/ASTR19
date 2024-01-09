470d51b4a0c975d1a3112d6c697962b27c10d137
atothsmi <atothsmi@ucsc.edu>
2024-01-09 12:57:25 -0800

    Updates and Comments to Session 1 and Session 2, Code Journal 1 Prompts

--- codeJournal_1/session1.py
+++ codeJournal_1/session1.py
@@ -1,4 +1,14 @@
-print("My name is Alia Toth-Smith.")
-print("My preferred pronouns are she/her.")
-print("My favorite movie is Knives Out.")
-print("My favorite dish is Tofu Chow Fun.")
+'''
+Program Prompt:
+Write a Python program to print out your full name, 
+your preferred pronouns (optional), and two sentences 
+about your favorite movie and your favorite food.
+'''
+def main():
+    print("My name is Alia Toth-Smith.")
+    print("My preferred pronouns are she/her.")
+    print("My favorite movie is Knives Out.")
+    print("My favorite dish is Tofu Chow Fun.")
+
+if __name__ == "__main__":
+    main()
--- codeJournal_1/session2.py
+++ codeJournal_1/session2.py
@@ -1,27 +1,42 @@
+'''
+Program Prompt:
+Write a Python program that prints the sum of two floating point numbers, 
+the difference between two integers, and the product of a floating point 
+number and an integer. In each case, have the program print out the data 
+type of the resulting answer.
+
+Extra Notes:
+This program also picks random floats and integers in a range [1, 100]
+to perform the calculations.
+'''
+#Gets random module
 import random
-int_list = [1, 4, 6]
-float_list = [2.0, 2.3, 8.9]
-operation_list = ['sum', 'difference', 'product']
-result = 0
 
-#Gets and stores 2 random floats and 2 random integers from their respective lists
-float1 = float_list[random.randint(0,2)]
-float2 = float_list[random.randint(0,2)]
-int1 = int_list[random.randint(0,2)]
-int2 = int_list[random.randint(0,2)]
+def main():
+    
+    #Gets and stores 2 random floats (rounded to 2 decimals) and 2 random integers
+    float1 = round(random.uniform(0, 100), 2)
+    float2 = round(random.uniform(0, 100), 2)
+    int1 = random.randint(0,100)
+    int2 = random.randint(0,100)
 
+    #Empty variable to store results of calculations later
+    result = 0
 
-for i in operation_list:
-    if i=='sum':
-        result = float1 + float2
-        print(f'The sum of {float1} and {float2} is {result}.')
-    elif i == 'difference':
-        result = int1 - int2
-        print(f'The difference between {int1} and {int2} is {result}.')
-    elif i == 'product':
-        result = round(float1 * int1, 1)
-        print(f'The product of {float1} and {int1} is {result}')
-    else:
-        #Error message
-        print("Uh oh something has gone wrong")
+    #Adds the 2 stored random floats
+    result = float1 + float2
+    print(f'The sum of {float1} and {float2} is {result}.')
     print(f'The data type of {result} is {type(result)}.')
+
+    #Subtracts the 2 stored random ints
+    result = int1 - int2
+    print(f'The difference between {int1} and {int2} is {result}.')
+    print(f'The data type of {result} is {type(result)}.')
+
+    #Multiplies the 1st stored float and the 1st stored int
+    result = round(float1 * int1, 2)
+    print(f'The product of {float1} and {int1} is {result}')
+    print(f'The data type of {result} is {type(result)}.')
+
+if __name__=="__main__":
+    main()
