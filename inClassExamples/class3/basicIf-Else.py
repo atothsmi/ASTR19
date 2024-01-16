3e5d32a70a47a65de458e27bf8e5ca3689f93680
atothsmi <atothsmi@ucsc.edu>
2024-01-16 13:12:13 -0800

    Update basicIf-Else.py

--- inClassExamples/class3/basicIf-Else.py
+++ inClassExamples/class3/basicIf-Else.py
@@ -5,6 +5,8 @@ def flow_control(k, a=0, b=10):
     # containing the requested values after the ending %
 
     # useful instead of f or .format
+    # because it saves a format that can substitute any variable values for 
+    # the % characters, instead of a nammed variable
     if(k==a):
         s = "Variable k = %d equals %d." % (k, a)
     elif(k > a and k < b):
@@ -16,6 +18,13 @@ def flow_control(k, a=0, b=10):
 
 def main():
     print(flow_control(0))
+    #{i:12d} print i with 12 spaces
+    #{i:012d} print i with 12 spaces, 0's added to the front
+    #{i:9.8e}
+
+    #reminder: e is equivalent to *10^
+    x = 123423414.2
+    print(f'{x:9.8e}')
 
 if __name__=="__main__":
     main()
